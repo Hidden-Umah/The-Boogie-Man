b64aee3ba14f42a401d218efe96e30940645e5a1
Hidden Umah <anonymousumah96@gmail.com>
2026-06-16 17:31:25 +0000

    Added the database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CarsService } from './cars/cars.service';
+import { CarsModule } from './cars/cars.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    CarsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+  
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CarsService],
 })
 export class AppModule {}
